4846bbd7279edd0d6be0324524f80ff7fcb1b815
Mattias Mandell <matma011@student.liu.se>
2024-03-01 14:12:45 +0100

    lade till en scen och background

---
+++ src/grafikPaket/Background.java
@@ -0,0 +1,12 @@
+package grafikPaket;
+
+import javafx.scene.canvas.Canvas;
+
+public class Background extends Canvas{
+
+	public Background() {
+		setWidth(1000);
+		setHeight(800);
+		setStyle("-fx-background-color: #a31239;"); 
+	}
+}
