392bb30dd802538ee41e74d21d68ffbd1c1a4f3a
Mattias Mandell <matma011@student.liu.se>
2024-03-01 14:56:35 +0100

    bytte till stort P

--- src/grafikPaket/MainScene.java
+++ src/grafikPaket/MainScene.java
@@ -1,5 +1,29 @@
 package grafikPaket;
 
-public class MainScene {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
+public class MainScene extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Plant on Zombie Warfare");
+		primaryStage.setWidth(1334); 
+		primaryStage.setHeight(774);
+		
+		HBox layout = new HBox();
+		Background canvas = new Background();
+		
+		layout.getChildren().add(layout);
+		layout.getChildren().add(canvas);
+		
+		Scene primaryScene = new Scene(layout);
+		primaryStage.setScene(primaryScene);
+		primaryStage.show();
+	}
 }
